af1de3a606fc57896e1fe39a0da16758a64777d9
917_denis_tvein <72063185+denistvein1@users.noreply.github.com>
2024-04-29 08:33:48 +0300

    lab4 th

---
+++ src/test/java/vvss/example/IntegrationTestTH.java
@@ -0,0 +1,116 @@
+package vvss.example;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import java.time.LocalDate;
+
+/**
+ * Unit test for simple App.
+ */
+public class IntegrationTestTH
+        extends TestCase {
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public IntegrationTestTH(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(IntegrationTestTH.class);
+    }
+
+    public void testAddStudent() {
+        Student testStudent = new Student("test_id", "Test Student", 933, "test_email@stud.ubbcluj.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e) {
+            fail();
+        }
+        service.deleteStudent("test_id");
+        assertTrue(true);
+    }
+
+    public void testAddAssignmentAndStudent() {
+        Tema testTema = new Tema("test_tema", "test_description", 1, 10);
+        try {
+            service.addTema(testTema);
+        } catch (Exception e) {
+            fail();
+        }
+        service.deleteTema("test_tema");
+
+        Student testStudent = new Student("test_id", "Test Student", 933, "test_email@stud.ubbcluj.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e) {
+            fail();
+        }
+        service.deleteStudent("test_id");
+        assertTrue(true);
+    }
+
+    public void testAddAll() {
+        Tema testTema = new Tema("test_tema", "test_description", 1, 10);
+        try {
+            service.addTema(testTema);
+        } catch (Exception e) {
+            fail();
+        }
+
+        Student testStudent = new Student("test_id", "Test Student", 933, "test_email@stud.ubbcluj.ro");
+        try {
+            service.addStudent(testStudent);
+        } catch (Exception e) {
+            service.deleteTema("test_tema");
+            fail();
+        }
+        assertTrue(true);
+
+        Nota testNota = new Nota("test_nota", "test_id", "test_tema", 9.0, LocalDate.parse("2024-02-02"));
+
+        try {
+            service.addNota(testNota, "test_feedback");
+        } catch (Exception e) {
+            System.out.print(e);
+
+            service.deleteTema("test_tema");
+            service.deleteStudent("test_id");
+            fail();
+        }
+        service.deleteTema("test_tema");
+        service.deleteStudent("test_id");
+        service.deleteNota("test_nota");
+        assertTrue(true);
+    }
+
+}
